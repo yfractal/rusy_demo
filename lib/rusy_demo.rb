290d8a9e5de93a0cb4741a58a9f7c2a2d42aa0d6
Mike Yang <yfractal@gmail.com>
2024-08-01 10:42:43 +0800

    feat: init

---
+++ lib/rusy_demo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rusy_demo/version"
+
+module RusyDemo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rusy_demo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RusyDemo do
+  it "has a version number" do
+    expect(RusyDemo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
